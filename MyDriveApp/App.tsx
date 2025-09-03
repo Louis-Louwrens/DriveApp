0c0d21667c906318c2b287c1e680542aca6116eb
Louis-Louwrens <ST10497905@vcconnect.edu.za>
2025-09-03 13:52:07 +0200

    added start of IF statement

--- MyDriveApp/App.tsx
+++ MyDriveApp/App.tsx
@@ -1,11 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  
+  const age = 16;
+  let feedback = '';
+  
+  if (age >= 18) {
+    feedback = 'You are eligible to drive. Yippie!!!';
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
+      
     </View>
   );
 }
