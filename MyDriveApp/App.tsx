fc998468581fa7f6bb97380073f2ea1825e2d5f4
Louis-Louwrens <ST10497905@vcconnect.edu.za>
2025-09-03 14:53:33 +0200

    End of class, added second IF

--- MyDriveApp/App.tsx
+++ MyDriveApp/App.tsx
@@ -4,16 +4,21 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   
   const age = 22;
+  const licenceYears = 5;
   let feedback = '';
   
   if (age >= 18) {
-    feedback = 'You are eligible to drive. Yippie!!!';
-  } else {
-    feedback = 'You too young for that small thing.';
-  }
+    if (licenceYears >= 3) {
+      feedback = 'You are a qualified, safe driver.';
+    } else 
+      feedback = 'You are eligible to drive. Yippie!!!';
+    } else {
+      feedback = 'You too young for that small thing.';
+    }
+  
   return (
     <View style={styles.container}>
-      <Text style={styles.response}>{feedback}</Text>
+      <Text style={styles.response}>{feedback}{/*This is my message to be displayed to the user */}</Text> 
       
     </View>
   );
@@ -22,7 +27,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#e9f09cff',
     alignItems: 'center',
     justifyContent: 'center',
   },
